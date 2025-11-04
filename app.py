ea451f93c78d6b29431e05dd48d7596270b4a6d3
nova <ikyemendez24@lhusurbil.eus>
2025-11-04 08:02:56 +0000

    Auth agregado

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from endpoints.clientes import clientesPlano, ventasPlano, contactosPlano
 from endpoints.auth import authPlano
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app, resources={r"/api/*":{"origins": "*"}})
 app.register_blueprint(clientesPlano,url_prefix="/api") #Con ek porefix le defino que para acceder a los planos se debe poner primero /api y luego su respectiva ruta
 app.register_blueprint(ventasPlano,url_prefix="/api")   
 app.register_blueprint(contactosPlano,url_prefix="/api")                #en este casp seria /clientes
